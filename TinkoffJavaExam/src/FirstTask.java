c0416067662d3e9d9d761ace383a271000882ec0
Carr Johnny <carrjohn@et-l6.kzn.21-school.ru>
2023-09-05 22:23:28 +0300

    exam done

---
+++ TinkoffJavaExam/src/FirstTask.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class FirstTask {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        long s = scanner.nextLong();
+
+        ArrayList<Long> prices = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            prices.add(scanner.nextLong());
+        }
+
+        prices.sort(Collections.reverseOrder());
+
+        long maxAffordablePrice = 0;
+
+        for (Long price : prices) {
+            if (price <= s) {
+                maxAffordablePrice = price;
+                break;
+            }
+        }
+
+        System.out.println(maxAffordablePrice);
+    }
+}
